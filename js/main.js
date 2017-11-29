3e5e136727d5da5673ab7066e4741691c7ddccae
Mike Turley <mike.turley@alum.cs.umass.edu>
2017-11-29 17:17:01 -0500

    defaults

--- js/main.js
+++ js/main.js
@@ -2,5 +2,6 @@
 document.addEventListener("DOMContentLoaded", function (event) {
     countdown(new Date('2018-08-25 20:00:00'), function (ts) {
         document.getElementById('countdown').innerHTML = ts.toHTML("strong");
-    }, countdown.HOURS | countdown.MINUTES | countdown.SECONDS);
+    },
+    countdown.DEFAULTS);
 });
