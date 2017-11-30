4bf85b95645cb1aff47398f34091700aac01bdab
Mike Turley <mike.turley@alum.cs.umass.edu>
2017-11-30 13:12:42 -0500

    aaaaaah

--- js/main.js
+++ js/main.js
@@ -1,12 +1,17 @@
+function pluralize(number, word) {
+    if (number === 1) return '1 ' + word;
+    return number + ' ' + word + 's';
+}
+
 
 document.addEventListener("DOMContentLoaded", function (event) {
     countdown.setLabels(null, null, '', '', null, null);
     countdown(new Date('2018-08-26T00:00:00.000Z'), function (ts) {
-        document.querySelector('#countdown .months').innerHTML = ts.months + ' months';
-        document.querySelector('#countdown .days').innerHTML = ts.days + ' days';
-        document.querySelector('#countdown .hours').innerHTML = ts.hours + ' hours';
-        document.querySelector('#countdown .minutes').innerHTML = ts.minutes + ' minutes';
-        document.querySelector('#countdown .seconds').innerHTML = ts.seconds + ' seconds';
+        document.querySelector('#countdown .months').innerHTML = pluralize(ts.months, 'month');
+        document.querySelector('#countdown .days').innerHTML = pluralize(ts.days, 'day');
+        document.querySelector('#countdown .hours').innerHTML = pluralize(ts.hours, 'hour');
+        document.querySelector('#countdown .minutes').innerHTML = pluralize(ts.minutes, 'minute');
+        document.querySelector('#countdown .seconds').innerHTML = pluralize(ts.seconds, 'second');
     },
     countdown.DEFAULTS);
 });
