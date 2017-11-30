e674ab82b0aff93542590e597c4e9f3751d3290c
Mike Turley <mike.turley@alum.cs.umass.edu>
2017-11-30 13:02:22 -0500

    aaaaaah

--- js/main.js
+++ js/main.js
@@ -1,8 +1,12 @@
 
 document.addEventListener("DOMContentLoaded", function (event) {
-    countdown.setLabels(null, null, '', '', ' - ', null);
+    countdown.setLabels(null, null, '', '', null, null);
     countdown(new Date('2018-08-26T00:00:00.000Z'), function (ts) {
-        document.getElementById('countdown').innerHTML = ts.toHTML('h2');
+        document.querySelector('#countdown .months').innerHTML = ts.months + ' months';
+        document.querySelector('#countdown .days').innerHTML = ts.days + ' days';
+        document.querySelector('#countdown .hours').innerHTML = ts.hours + ' hours';
+        document.querySelector('#countdown .minutes').innerHTML = ts.minutes + ' minutes';
+        document.querySelector('#countdown .seconds').innerHTML = ts.seconds + ' seconds';
     },
     countdown.DEFAULTS);
 });
