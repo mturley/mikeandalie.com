cefe25976f597a083fb7933feaf3f02aa5043d76
Mike Turley <mike.turley@alum.cs.umass.edu>
2018-05-26 13:14:06 -0400

    Add content to new sections

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import ImageGallery from 'react-image-gallery';
+import Back2Top from 'react-back2top';
 import scrollToComponent from 'react-scroll-to-component';
 import countdown from './vendor/countdown';
 import logo from './logo.svg';
@@ -102,16 +103,16 @@ class App extends Component {
                       onClick={this.scrollToDetails}
                       href="#"
                     >
-                      <h2>Details</h2>
+                      <h2 className="cursive">Venue</h2>
                     </a>
                     <a onClick={this.scrollToRegistry} href="#">
-                      <h2>Registries</h2>
+                      <h2 className="cursive">Registries</h2>
                     </a>
                     <a onClick={this.scrollToRSVP} href="#">
-                      <h2>RSVP</h2>
+                      <h2 className="cursive">RSVP</h2>
                     </a>
                     <a onClick={this.scrollToGallery} href="#">
-                      <h2>Memories</h2>
+                      <h2 className="cursive">Memories</h2>
                     </a>
                   </div>
                 </section>
@@ -119,26 +120,57 @@ class App extends Component {
             </div>
           </div>
         </div>
+        <Back2Top>
+          <div className="back-to-top">
+            <a href="#">Back to Top</a>
+          </div>
+        </Back2Top>
         <div
           className="fullscreen details-container"
           ref={r => this._detailsContainer = r}
         >
-          <h1>DETAILS GO HERE</h1>
-          <p>Stuff and things</p>
+          <h1>Venue</h1>
+          <h2>
+            Hyland Orchard &amp; Brewery<br />
+            199 Arnold Rd, Sturbridge, MA 01518
+          </h2>
+          <p>
+            We are thrilled for you to join us for our ceremony on the sunny hilltop at Hyland Orchard.<br />
+            Wagon rides to the ceremony will begin at 2:30 PM.
+          </p>
+          <p>
+            A reception will follow, to be held in the barn and bonfire area on site.
+          </p>
+          <p>
+            A brewery and disc golf course are on site, and lawn games will be available.
+          </p>
         </div>
         <div
           className="fullscreen registry-container"
           ref={r => this._registryContainer = r}
         >
-          <h1>REGISTRY GOES HERE</h1>
-          <p>Stuff and things</p>
+          <h1>Registries</h1>
+          <p>We are registered at Amazon and at Bed Bath &amp; Beyond.</p>
+          <p>
+            Amazon Registry:&nbsp;
+            <a href="https://www.amazon.com/wedding/share/mikeandalie" target="_blank">
+              https://www.amazon.com/wedding/share/mikeandalie
+            </a>
+          </p>
+          <p>
+            Bed Bath &amp; Beyond Registry:&nbsp;
+            <a href="https://www.bedbathandbeyond.com:443/store/giftregistry/view_registry_guest.jsp?registryId=545629397&eventType=Wedding" target="_blank">
+              https://www.bedbathandbeyond.com:443/store/giftregistry/view_registry_guest.jsp?registryId=545629397&eventType=Wedding
+            </a>
+          </p>
         </div>
         <div
           className="fullscreen rsvp-container"
           ref={r => this._rsvpContainer = r}
         >
-          <h1>RSVP STUFF GO HERE</h1>
-          <p>Stuff and things</p>
+          <h1>RSVP</h1>
+          <p>An RSVP form will appear here soon.</p>
+          <p>For now, you may RSVP by contacting Mike or Alie.</p>
         </div>
         <div
           className="fullscreen gallery-container"
