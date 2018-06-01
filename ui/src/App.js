85ae0e7ef42a0ec9210fe1d0c46fc3e240e4755d
Mike Turley <mike.turley@alum.cs.umass.edu>
2018-05-31 23:46:19 -0400

    Move to lerna and add express api package

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import Back2Top from 'react-back2top';
 import scrollToComponent from 'react-scroll-to-component';
-import logo from './logo.svg';
 
 import './App.css';
 import '../node_modules/react-image-gallery/build/image-gallery.css';
