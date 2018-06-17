548f9105eb4d883596bb728778ac3526936c4f0b
Mike Turley <mike.turley@alum.cs.umass.edu>
2018-06-17 00:14:23 -0400

    Fix things with rsvp so far

---
+++ rsvp/imports/ui/RSVPApp.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className="container">
+        <header>
+          <h1>Mike &amp; Alie</h1>
+        </header>
+
+        <h2>TODO: Form.</h2>
+      </div>
+    );
+  }
+}
