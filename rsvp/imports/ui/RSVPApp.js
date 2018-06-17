3012ffbb8f98a6cf9fd9bded00720f0d2c113bd9
Mike Turley <mike.turley@alum.cs.umass.edu>
2018-06-17 17:48:01 -0400

    Add common css and stuff for code

--- rsvp/imports/ui/RSVPApp.js
+++ rsvp/imports/ui/RSVPApp.js
@@ -13,8 +13,12 @@ export default class RSVPApp extends Component {
               Please respond no later than July 25th.
             </p>
             <form>
-              <h2>Form elements go in here</h2>
+              <p>Enter the 4-digit code on your RSVP card:</p>
+              <h1 className="code">1234</h1>
             </form>
+            <p>
+              <small><a href="#">Lost your code? Like filling out forms?</a></small>
+            </p>
           </div>
         </div>
       </div>
