d5939aa4f763fd5d49824c98c2a592d275b374b3
Mike Turley <mike.turley@alum.cs.umass.edu>
2018-06-17 23:12:54 -0400

    RSVP look and feel done?

--- rsvp/imports/ui/RSVPApp.js
+++ rsvp/imports/ui/RSVPApp.js
@@ -1,27 +1,42 @@
 import React, { Component } from 'react';
 
 export default class RSVPApp extends Component {
+  constructor() {
+    super();
+    this.state = {
+      code: ''
+    };
+    this.onCodeChange = this.onCodeChange.bind(this);
+  }
+
+  onCodeChange(event) {
+    const { target: { value } } = event;
+    this.setState({ code: value.substring(0, 4) });
+  }
+
   render() {
+    const { code } = this.state;
     return (
-      <div className="container">
+      <main>
         <header>
           <h1>RSVP</h1>
+          <h3>0 confirmed guests</h3>
         </header>
-        <div className="form-container">
-          <div>
+        <section className="form-container">
+          <form>
+            <p>Enter the 4-digit code found on your RSVP card:</p>
+            <input className="code" value={code} onChange={this.onCodeChange} />
             <p>
-              Please respond no later than July 25th.
+              <a className="no-code" href="#">Lost your code? Love filling out forms?</a>
             </p>
-            <form>
-              <p>Enter the 4-digit code on your RSVP card:</p>
-              <h1 className="code">1234</h1>
-            </form>
-            <p>
-              <small><a href="#">Lost your code? Like filling out forms?</a></small>
-            </p>
-          </div>
-        </div>
-      </div>
+          </form>
+        </section>
+        <footer>
+          <h4>
+            Please respond no later than July 25th.
+          </h4>
+        </footer>
+      </main>
     );
   }
 }
