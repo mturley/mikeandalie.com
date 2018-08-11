e935721b3ea9f17a3675093b3bccb0b5fad77189
Mike Turley <mturley@redhat.com>
2018-08-11 15:01:02 -0400

    Add missing times file

---
+++ home/src/components/times.js
@@ -0,0 +1,3 @@
+export const CEREMONY_START_TIME = new Date('Sat Aug 25 2018 14:30:00 GMT-0400');
+export const RECEPTION_START_TIME = new Date('Sat Aug 25 2018 17:30:00 GMT-0400');
+export const MORNING_AFTER_TIME = new Date('Sun Aug 26 2018 09:00:00 GMT-0400');
