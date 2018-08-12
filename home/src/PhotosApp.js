92d09ee4ba5c8e586bb0c6fb65de3497a8191b7c
Mike Turley <mturley@redhat.com>
2018-08-12 12:17:47 -0400

    Remove upload form

--- home/src/PhotosApp.js
+++ home/src/PhotosApp.js
@@ -10,16 +10,12 @@ const PhotosApp = () => (
     </header>
     <h1>Wedding Guest Photos</h1>
     <h3 className="no-top-margin no-bottom-margin">
-      If you've taken photos at our wedding, we want to see them and share them!<br />
-      There are two ways to send us your photos:
+      If you've taken photos at our wedding, we want to see them and share them!
     </h3>
     <div style={{ margin: 20 }}>
       <div className="photos-box">
         <p>
-          Option 1: <strong><a href="https://goo.gl/forms/OJ83CjdPsxMLmwst1">Click here to upload photos directly using our Google form.</a></strong>
-        </p>
-        <p className="no-top-margin">
-          Option 2: Email your photos to <strong><a href="mailto:photos@mikeandalie.com">photos@mikeandalie.com</a></strong>.
+          Please email your photos to <strong><a href="mailto:photos@mikeandalie.com">photos@mikeandalie.com</a></strong>.
         </p>
       </div>
     </div>
